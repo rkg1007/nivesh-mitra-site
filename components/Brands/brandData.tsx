54e2f522a039bf0997877123dbcb40283c1aec4f
rohit <rkg10072000@gmail.com>
2024-09-11 23:36:57 +0530

    initial commit

---
+++ components/Brands/brandData.tsx
@@ -0,0 +1,24 @@
+const brandData: any[] = [
+  {
+    id: 0.25,
+    title: '₹1,000',
+    desciption: 'Minimum Investment'
+  },
+  {
+    id: 0.3,
+    title: '₹1,115Cr',
+    desciption: 'Investments Enabled'
+  },
+  {
+    id: 0.4,
+    title: '4Lac+',
+    desciption: 'Investor Community'
+  },
+  {
+    id: 0.5,
+    title: '0.2%',
+    desciption: 'Default'
+  },
+];
+
+export default brandData;
---
+++ components/FeaturesTab/FeaturesTabItem.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FeatureTab } from "@/types/featureTab";
+import Image from "next/image";
+
+const FeaturesTabItem = ({ featureTab }: { featureTab: FeatureTab }) => {
+  const { title, desc1, desc2, image, imageDark } = featureTab;
+
+  return (
+    <>
+      <div className="flex items-center gap-8 lg:gap-19">
+        <div className="md:w-1/2">
+          <p className="mb-5">{desc1}</p>
+          <p className="w-11/12">{desc2}</p>
+        </div>
+        <div className="relative mx-auto hidden aspect-[562/366] max-w-[550px] md:block md:w-1/2">
+          <Image src={image} alt={title} fill className="dark:hidden" />
+          <Image
+            src={imageDark}
+            alt={title}
+            fill
+            className="hidden dark:block"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FeaturesTabItem;
