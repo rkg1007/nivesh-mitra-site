0e8b3a8e6ae1e456fdfa43f6170c4a78912aaba9
rohit <rkg10072000@gmail.com>
2024-09-29 20:58:05 +0530

    fixed blogs redirection link

--- src/components/Common/BlogCard.tsx
+++ src/components/Common/BlogCard.tsx
@@ -3,48 +3,39 @@ import Link from "next/link";
 
 const BlogCard = ({ blog }: any) => {
   return (
-    <div className="group">
-      <div className="relative mb-6 aspect-[370/280] w-full overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-        <Link href={"/blogs/1"}>
+    <Link href={"/blogs/1"}>
+      <div className="group">
+        <div className="relative mb-6 aspect-[370/280] w-full overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
           <Image
             src={blog.image}
             alt={"Some Alt Text"}
             fill
             className="w-full object-cover"
           />
-        </Link>
-      </div>
+        </div>
 
-      <h3>
-        <Link
-          href={"/blog-details/blog-details-one"}
-          className="mb-3.5 block text-xl font-bold text-dark"
-        >
+        <h3>
           <span className="bg-gradient-to-r from-primary/20 to-primary/10 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
             {blog.title}
           </span>
-        </Link>
-      </h3>
-      <p>{blog.description}</p>
+        </h3>
+        <p>{blog.description}</p>
 
-      <div className="mt-4.5 flex flex-wrap items-center justify-between gap-3">
-        <div className="flex items-center gap-2.5">
-          <p className="text-sm">
-            {new Date(blog.publishedAt)
-              .toDateString()
-              .split(" ")
-              .slice(1)
-              .join(" ")}
-          </p>
-        </div>
-        <Link
-          href={`/category/finance`}
-          className="inline-flex rounded-full bg-blue/[0.08] px-3 py-1 text-sm font-medium capitalize text-blue"
-        >
+        <div className="mt-4.5 flex flex-wrap items-center justify-between gap-3">
+          <div className="flex items-center gap-2.5">
+            <p className="text-sm">
+              {new Date(blog.publishedAt)
+                .toDateString()
+                .split(" ")
+                .slice(1)
+                .join(" ")}
+            </p>
+          </div>
+
           {blog.category}
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
